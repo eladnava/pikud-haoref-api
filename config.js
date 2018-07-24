c7bc1a01b6589410d8ffeeaca8707318c0c5924c
Elad Nava <eladnava@gmail.com>
2018-07-24 21:10:02 +0800

    Fix broken config file URL references

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ module.exports = {
             api: 'http://www.oref.org.il/WarningMessages/Alert/alerts.json?v=1'
         },
         // URLs to Pikud Haoref's official homepages in Hebrew and English which contain city metadata
-        metadata: {
-            heb: 'http://www.oref.org.il/11093-he/Pakar.aspx',
-            eng: 'http://www.oref.org.il/894-en/Pakar.aspx'
+        website: {
+            hebUrl: 'http://www.oref.org.il/11093-he/Pakar.aspx',
+            engUrl: 'http://www.oref.org.il/894-en/Pakar.aspx'
         }
     }
 };
