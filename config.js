1b7609155b1955aa631309c9d6a55aec816af171
Elad Nava <eladnava@gmail.com>
2019-07-02 16:10:38 +1200

    Updated HTTPS endpoints for cities.json crawler

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ module.exports = {
         },
         // URLs to Pikud Haoref's official homepages in Hebrew and English which contain city metadata
         website: {
-            hebUrl: 'http://www.oref.org.il/11093-he/Pakar.aspx',
-            engUrl: 'http://www.oref.org.il/894-en/Pakar.aspx'
+            hebUrl: 'https://www.oref.org.il/11093-he/Pakar.aspx',
+            engUrl: 'https://www.oref.org.il/894-en/Pakar.aspx'
         }
     },
     israel: {
